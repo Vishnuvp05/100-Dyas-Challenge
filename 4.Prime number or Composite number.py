2d7cf32b7dc65f825f997246b99d8f0347186978
Vishnuvp05 <111220589+Vishnuvp05@users.noreply.github.com>
2024-01-30 11:14:31 +0530

    Update 4.Prime number or Composite number.py

--- 4.Prime number or Composite number.py
+++ 4.Prime number or Composite number.py
@@ -1,7 +1,11 @@
+#define a function named is_prime and pass number
 def is_prime(number):
+    # 1 is a not prime so if to selct less than 2
     if number < 2:
         return True
-    for i in range(2, int(number**0.5) + 1):
+    #for i in range 2 to square root of the number 
+    for i in range(2, int(number**0.5) + 1)
+    #if the range number is divisable and remainder is zero returns true 
         if number % i == 0:
             return True
     return False
