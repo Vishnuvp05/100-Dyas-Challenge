86fa0086811c66c4fc9b48257c7745f108e803e4
Vishnuvp05 <111220589+Vishnuvp05@users.noreply.github.com>
2024-01-30 11:11:29 +0530

    Add files via upload

---
+++ 5.Reverse a string.py
@@ -0,0 +1,9 @@
+#define  a function reverse_string 
+def reverse_string(string):
+    #the code returns the string in reverse order 
+    return string[::-1]
+
+# Test the function
+text = "Hello, World!"
+reversed_text = reverse_string(text)
+print(reversed_text)
