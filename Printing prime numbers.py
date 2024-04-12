203e15755fc900902ca8cc9d8209643c9c7ceaba
VISHNUPRASATH <111220589+Vishnuvp05@users.noreply.github.com>
2024-04-12 16:29:48 +0530

    Update Printing prime numbers.py

--- Printing prime numbers.py
+++ Printing prime numbers.py
@@ -35,3 +35,21 @@ def is_prime(n):
 prime_list = [x for x in range(2, 101) if is_prime(x)]
 print(prime_list)
 
+
+
+def is_prime(number):
+    # 1 is a not prime so if to selct less than 2
+    if number < 2:
+        return False
+    #for i in range 2 to square root of the number 
+    for i in range(2, int(number**0.5) + 1):
+    #if the range number is divisable and remainder is zero returns true 
+        if number % i == 0:
+            return False
+    return True
+
+
+a=[8,42,2,4,5,7,8,555,6,7,5,432,343,3,4,4,54,2,2]
+b=[i for i in a if is_prime(i)]
+print(b)
+
