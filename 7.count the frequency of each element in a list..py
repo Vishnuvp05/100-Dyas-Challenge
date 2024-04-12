63bff413e83bb9263d6da121321a6c58b6fdef8a
VISHNUPRASATH <111220589+Vishnuvp05@users.noreply.github.com>
2024-04-12 16:50:08 +0530

    Update 7.count the frequency of each element in a list..py

--- 7.count the frequency of each element in a list..py
+++ 7.count the frequency of each element in a list..py
@@ -14,3 +14,29 @@ def count_frequency(numbers):
 nums = [1, 2, 3, 2, 1, 3, 2, 4, 5, 4]
 frequency_count = count_frequency(nums)
 print(frequency_count)
+
+
+a=[1,3,22,12,3,11,3,1,3,1,1]
+
+b={}
+
+for i in a:
+    if i in b:
+        b[i]+=1
+    else:
+        b[i]=1
+
+maxi=max(b,key=b.get)
+print(b)
+
+#or
+
+max_val=float('-inf')
+max_key=None
+
+for key,value in b.items():
+    if value>max_val:
+        max_val=value
+        max_key=key
+
+print(max_key,max_val)
