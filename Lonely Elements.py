7689c3b721b11ff7bada8654d247cbf4ba3f5375
VISHNUPRASATH <111220589+Vishnuvp05@users.noreply.github.com>
2024-04-12 17:29:20 +0530

    Update Lonely Elements.py

--- Lonely Elements.py
+++ Lonely Elements.py
@@ -1,4 +1,5 @@
 # Given an array of integers, where all elements but one occur twice, find the unique element.
+#only possible if one lonely and others occurs most twice or divide of two
 
 a=[1,22,33,22,33,4,4,5,5,5,5,6,6,1,9]
 r=0
