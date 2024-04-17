637cfa4d827534e882b66eff6acc20865c4bf19c
VISHNUPRASATH <111220589+Vishnuvp05@users.noreply.github.com>
2024-04-17 13:33:04 +0530

    Add files via upload

---
+++ Square and rectangle.py
@@ -0,0 +1,19 @@
+#rectangle
+for i in range(2): #this first loop as row 
+    for j in range(10):#this second loop as coloumn 
+        print('*',end=' ')
+    print() #to move to next line after printing first coloumn
+
+# * * * * * * * * * * 
+# * * * * * * * * * *
+
+#sqaure 
+for i in range(5):
+    for j in range(5):
+        print('*',end=' ')
+    print()
+
+# * * * * *
+# * * * * *
+# * * * * *
+# * * * * *
