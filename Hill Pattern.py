b405a56b1b2a0ca1271cad9f21ba69ceb5aaa990
VISHNUPRASATH <111220589+Vishnuvp05@users.noreply.github.com>
2024-04-17 14:19:52 +0530

    Update Hill Pattern.py

--- Hill Pattern.py
+++ Hill Pattern.py
@@ -4,7 +4,7 @@ n=5
 for i in range(n):
     for j in range(i,n):
         print(' ',end=' ')
-    for j in range(i):
+    for j in range(i): #here we are changing i+1 to i because to avoif middle repitition
         print('*',end=' ')
     for j in range(i+1):
         print('*',end=' ')
