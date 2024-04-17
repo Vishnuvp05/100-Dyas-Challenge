399d9169bd680a33fca9e1a50ac091fda27edb93
VISHNUPRASATH <111220589+Vishnuvp05@users.noreply.github.com>
2024-04-17 13:58:38 +0530

    Add files via upload

---
+++ Right Sided Triangle.py
@@ -0,0 +1,9 @@
+#Right Sided Triangle
+#which is a combination of decresing space and increasing triangle
+n=5
+for i in range(n):
+    for j in range(i,n):
+        print(" ", end=' ')
+    for j in range(i+1):
+        print('*',end=' ')
+    print()
